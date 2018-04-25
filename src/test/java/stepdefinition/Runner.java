91ce072495719db0375b590ce687bbc549baf723
teamlead11 <automationlead11@gmail.com>
2018-04-25 09:36:51 +0530

    some chnages

--- src/test/java/stepdefinition/Runner.java
+++ src/test/java/stepdefinition/Runner.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "Feature", glue = { "stepdefinition" }, tags = { "@textbox,@radio,@radio" }, plugin = {
-		"pretty", "json:/src/resources/java/cucumber-json-report" })
+		"pretty", "json:/src/resources/java/cucumber-json-report.json" })
 
 public class Runner {
 
