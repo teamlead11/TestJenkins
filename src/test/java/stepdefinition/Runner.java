f949edd645588061200488b523f92ed91c7558b0
teamlead11 <33974096+teamlead11@users.noreply.github.com>
2018-09-04 10:44:05 +0530

    test changes

--- src/test/java/stepdefinition/Runner.java
+++ src/test/java/stepdefinition/Runner.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "Feature", glue = { "stepdefinition" }, tags = { "@textbox,@radio,@radio" }, plugin = {
+@CucumberOptions(features = "Feature", glue = { "stepdefinition" }, tags = { "@textbox,@radio,@dropdown" }, plugin = {
 		"pretty", "json:/src/resources/java/cucumber-json-report.json" })
 
 public class Runner {
